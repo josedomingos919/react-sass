ef150be0e6094bf4ae7dab5d6de3490420bf847c
josedomingos919 <josedomingos919@gmail.com>
2022-07-17 23:48:34 +0100

    feat: add libs react-icons and sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./styles/main.sass";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
